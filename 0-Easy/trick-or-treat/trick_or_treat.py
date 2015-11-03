997a4a25278d69064211f679104f895c60beb870
Alexander Temnikov <alexander.temnikov@me.com>
2015-11-03 09:33:48 +0300

    Removed replace, split changed

--- 0-Easy/trick-or-treat/trick_or_treat.py
+++ 0-Easy/trick-or-treat/trick_or_treat.py
@@ -2,6 +2,6 @@ import sys
 
 
 for line in open(sys.argv[1]):
-    vampires, zombies, witches, houses = [int(l.split(":")[1]) for l in line.rstrip().replace(" ", "").split(",")]
+    vampires, zombies, witches, houses = [int(l.split(": ")[1]) for l in line.rstrip().split(", ")]
     candies = int((vampires * 3 + zombies * 4 + witches * 5) * houses / (vampires + zombies + witches))
     print(candies)
