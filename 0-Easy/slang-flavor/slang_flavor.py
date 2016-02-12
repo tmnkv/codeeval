a27399a487e06c7f8dad21f4e92893ec226440da
Alexander Temnikov <alexander.temnikov@me.com>
2016-02-12 13:27:24 +0300

    Solution for Slang Flavor problem

---
+++ 0-Easy/slang-flavor/slang_flavor.py
@@ -0,0 +1,14 @@
+import sys
+
+slang = [
+    ", yeah!",
+    ", this is crazy, I tell ya.",
+    ", can U believe this?",
+    ", eh?",
+    ", aw yea.",
+    ", yo.",
+    "? No way!",
+    ". Awesome!"
+]
+
+for line in open("input.txt"):
