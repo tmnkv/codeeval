386324ffa68b01034dcf75f32b1b68bd48c5d0bb
Alexander Temnikov <alexander.temnikov@me.com>
2016-02-12 13:14:38 +0300

    Solution for Football problem

---
+++ 0-Easy/football/football.py
@@ -0,0 +1,34 @@
+import sys
+
+
+def fillTheSet(data):
+    teams = set()
+    for i in range(len(data)):
+        teams = teams.union({int(x) for x in data[i]})
+    return teams
+
+
+def fanSearch(team, data):
+    result = []
+    for number in range(len(data)):
+        if str(team) in data[number]:
+            result.append(str(number + 1))
+    return result
+
+
+def main(inpit_file):
+    fh = open(inpit_file, mode='r')
+    for line in fh:
+        data = line.rstrip().split(' | ')
+        data = [x.split() for x in data]
+        teams = fillTheSet(data)
+        for team in sorted(teams):
+            fans = fanSearch(team, data)
+            print('{0}:{1};'.format(team, ','.join(fans)), end=' ')
+        print()
+
+# if __name__ == '__main__':
+#     if len(sys.argv) != 2:
+#         print("Usage: {0} <input_file>".format(sys.argv[0]))
+#     main(sys.argv[1])
+main('/Users/tmnkv/GitHub/codeeval/0-Easy/football/input.txt')
