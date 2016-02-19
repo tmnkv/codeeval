c200ff9f8ccae6243aff0004fd933c2d8a63caaf
Alexander Temnikov <alexander.temnikov@me.com>
2016-02-19 23:14:20 +0300

    Fixed solution for Slang Flavor problem

--- 0-Easy/slang-flavor/slang_flavor.py
+++ 0-Easy/slang-flavor/slang_flavor.py
@@ -1,6 +1,7 @@
 import sys
+import re
 
-slang = [
+SLANG = [
     ", yeah!",
     ", this is crazy, I tell ya.",
     ", can U believe this?",
@@ -11,4 +12,25 @@ slang = [
     ". Awesome!"
 ]
 
-for line in open("input.txt"):
+
+def main(input_file):
+    fh = open(input_file)
+    flag = False
+    count = 0
+    for line in fh:
+        sentences = [x for x in re.split('[.?!]', line.rstrip()) if len(x) >0]
+        result = ''
+        for sentence in sentences:
+            if flag:
+                result += sentence + SLANG[count % len(SLANG)]
+                count += 1
+                flag = False
+            else:
+                result += sentence + line[line.index(sentence) + len(sentence)]
+                flag = True
+        print(result)
+
+
+
+
+main(sys.argv[1])
